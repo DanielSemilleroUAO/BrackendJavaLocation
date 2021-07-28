e2a2de6a0c284ea2215dc4ddeebda0ba050f17cf
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-07-28 09:12:49 -0500

    Inicio del proyecto

---
+++ src/main/java/com/daniel/BackendJavaLocationApplication.java
@@ -0,0 +1,13 @@
+package com.daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaLocationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/daniel/entity/Location.java
@@ -0,0 +1,5 @@
+package com.daniel.entity;
+
+public class Location {
+
+}
