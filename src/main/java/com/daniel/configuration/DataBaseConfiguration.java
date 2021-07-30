23162e2197ef7c053b1fad57f3a9b880515b3706
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-07-30 10:23:28 -0500

    Se agrega archivos para crear docker-compose con mysqldb

--- src/main/java/com/daniel/configuration/DataBaseConfiguration.java
+++ src/main/java/com/daniel/configuration/DataBaseConfiguration.java
@@ -31,9 +31,9 @@ public class DataBaseConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/datalocations");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/datalocations?allowPublicKeyRetrieval=true&useSSL=false");
 		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setPassword("root");
 		return dataSource;
 	}
 	
