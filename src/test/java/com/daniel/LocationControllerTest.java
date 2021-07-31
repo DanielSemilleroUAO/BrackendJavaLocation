e0a0f42ade568136ba984ace036de4466ebdf544
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-07-31 07:41:54 -0500

    Se agrega caso de prueba

--- src/test/java/com/daniel/LocationControllerTest.java
+++ src/test/java/com/daniel/LocationControllerTest.java
@@ -1,7 +1,9 @@
 package com.daniel;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -11,19 +13,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.daniel.entity.Location;
+import com.daniel.repository.LocationDao;
+import com.daniel.service.LocationService;
+import com.daniel.service.LocationServiceImpl;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class LocationControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@InjectMocks
+	LocationServiceImpl locationService;
+	@Mock
+	LocationDao dataServiceMock;
+	
+	@Test
+	public void calculateSumUsingDataService_basic() {
+		when(dataServiceMock.findById((long) 1)).thenReturn(new Location("Cali",50));
+		assertEquals("Cali", locationService.findById((long) 1).getName());
+	}
+	
 	/*
 	@Test
 	public void locationControllerTest_getAllLocations() throws Exception {
